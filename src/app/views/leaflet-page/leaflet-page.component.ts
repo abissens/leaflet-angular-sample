a5d77716daa1311a9ce5cf89d4e51c1fd3df71a9
Abissens <hichem.abissens@gmail.com>
2024-07-11 22:22:36 +0200

    implementing first page

--- src/app/views/leaflet-page/leaflet-page.component.ts
+++ src/app/views/leaflet-page/leaflet-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {Map, map, tileLayer} from 'leaflet';
 
 @Component({
   selector: 'app-leaflet-page',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './leaflet-page.component.html',
   styleUrl: './leaflet-page.component.less'
 })
-export class LeafletPageComponent {
+export class LeafletPageComponent implements AfterViewInit {
+  private map!: Map;
+
+  ngAfterViewInit(): void {
+    this.map = map('map', {
+      center: [36.83359606938916, 10.147116233476737],
+      zoom: 3
+    });
+
+    const tiles = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 30,
+      minZoom: 18
+    });
+
+    tiles.addTo(this.map);
+  }
 
 }
